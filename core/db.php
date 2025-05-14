fd46c0f98968ef8bf7c759305f548c508e7ab5a6
Mohammad <162719285+moded12@users.noreply.github.com>
2025-05-14 12:14:30 +0300

    Delete core directory

--- core/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$user = 'edu.project';
-$pass = 'Tvvcrtv1610@';
-$dbname = 'edu.project';
-$conn = new mysqli($host, $user, $pass, $dbname);
-mysqli_set_charset($conn, 'utf8mb4');
-?>
